12a3009b417297c3d3f933a001414c5698564aed
Bruno Otávio Souza de Ávila <92824908+BrunoSA89@users.noreply.github.com>
2023-02-03 15:58:05 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/githubproject/IdTela.kt
@@ -0,0 +1,11 @@
+package com.example.githubproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class IdTela : Fragment(R.layout.fragment_id_tela) {
+
+}
